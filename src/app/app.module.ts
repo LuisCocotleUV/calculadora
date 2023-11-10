9efd0f425d035c26d32d5cebcb88577f4e3abc30
Cocotle <luisdavidcocoyuv@gmail.com>
2023-11-09 21:06:03 -0600

    initial commit - structure and look

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UiComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, UiComponent]
 })
 export class AppModule { }
---
+++ src/app/exp/exp.ts
@@ -0,0 +1,1 @@
+// Exponenciación
