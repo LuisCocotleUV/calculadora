20bb99664954e62525756828edeed28e65e2091a
Cocotle <luisdavidcocoyuv@gmail.com>
2023-11-10 10:55:29 -0600

    syntax fix

--- src/app/addition/addition.spec.ts
+++ src/app/addition/addition.spec.ts
@@ -1,17 +1,17 @@
 import { Addition } from './addition';
 
 describe('Addition', () => {
-  it('should return the sum of two numbers', () => {
+  it('should return 3 if num1 = 1 and num2 = 2', () => {
     const result = Addition.calculate(1, 2);
     expect(result).toBe(3);
   });
 
-  it('should return the sum of negative numbers', () => {
+  it('should return -3 if num1 = -1 and num2 = -2', () => {
     const result = Addition.calculate(-1, -2);
     expect(result).toBe(-3);
   });
 
-  it('should return the sum of a number and zero', () => {
+  it('should return 5 if num1 = 5 and num2 = 0', () => {
     const result = Addition.calculate(5, 0);
     expect(result).toBe(5);
   });
